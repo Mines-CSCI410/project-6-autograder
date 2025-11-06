5eed4b25a7f54da35ba5dcc45763f679643d5330
Grant Lemons <grantlemons@aol.com>
2025-11-05 18:38:02 -0700

    rename runTest so it isn't considered a test

--- src/tests/test_modules.py
+++ src/tests/test_modules.py
@@ -15,7 +15,7 @@ class TestBase(unittest.TestCase):
         if res != 0:
             raise AssertionError('Assembler output does not mach the expected output!')
 
-    def runTest(self, name):
+    def assertCorrectAssemble(self, name):
         self.runStudentCode(name)
         self.assertNoDiff(name)
 
@@ -23,34 +23,34 @@ class TestModules(TestBase):
     @weight(95/7)
     @number(1)
     def test_add(self):
-        self.runTest('Add')
+        self.assertCorrectAssemble('Add')
 
     @weight(95/7)
     @number(2)
     def test_max(self):
-        self.runTest('Max')
+        self.assertCorrectAssemble('Max')
 
     @weight(95/7)
     @number(3)
     def test_maxL(self):
-        self.runTest('MaxL')
+        self.assertCorrectAssemble('MaxL')
 
     @weight(95/7)
     @number(4)
     def test_pong(self):
-        self.runTest('Pong')
+        self.assertCorrectAssemble('Pong')
 
     @weight(95/7)
     @number(5)
     def test_pongL(self):
-        self.runTest('PongL')
+        self.assertCorrectAssemble('PongL')
 
     @weight(95/7)
     @number(6)
     def test_rect(self):
-        self.runTest('Rect')
+        self.assertCorrectAssemble('Rect')
 
     @weight(95/7)
     @number(6)
     def test_rectL(self):
-        self.runTest('RectL')
+        self.assertCorrectAssemble('RectL')
