1ddbee22f75897cec95e83c97fe59153e5c89824
Grant Lemons <grantlemons@aol.com>
2026-05-09 18:32:39 -0600

    standardizing the way run_student_code is being called with other projects

--- run_student_code.sh
+++ run_student_code.sh
@@ -5,6 +5,6 @@ EXECUTABLE=./Assembler
 pushd /autograder/source/ >/dev/null
 
 # run student-submitted code (untrusted)
-runuser -u student -- ${EXECUTABLE} $(pwd)/${1}
+runuser -u student -- ${EXECUTABLE} $(pwd)/${1}.asm
 
 popd >/dev/null
