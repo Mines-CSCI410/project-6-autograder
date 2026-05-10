bf24e7240ec2bf375d2b48962adcd4a155959756
Grant Lemons <grantlemons@aol.com>
2026-05-09 18:24:26 -0600

    do not move hack files to outputs

--- run_student_code.sh
+++ run_student_code.sh
@@ -7,7 +7,4 @@ pushd /autograder/source/ >/dev/null
 # run student-submitted code (untrusted)
 runuser -u student -- ${EXECUTABLE} $(pwd)/${1}
 
-# copy outputs to own directory
-mv *.hack /autograder/outputs
-
 popd >/dev/null
